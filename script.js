545f1fb53ebb49ea7cd2bfd8d1721fd7fd0eca12
ashashlin <104874547+ashashlin@users.noreply.github.com>
2025-01-06 15:56:33 +0800

    update charSet when different specs are selected

--- script.js
+++ script.js
@@ -6,14 +6,61 @@ charLengthSlider.addEventListener("input", () => {
   charLengthNum.innerHTML = charLength;
 });
 
-function charLengthZero() {
-  const charLength = Number(charLengthSlider.value);
+function passwordPlaceholderStyle() {
   const password = document.querySelector(".js-password");
   const copyPassword = document.querySelector(".js-copy-password");
 
-  if (charLength === 0) {
-    password.style.opacity = ".2";
-    copyPassword.style.pointerEvents = "none";
-  }
+  password.style.opacity = ".2";
+  copyPassword.style.pointerEvents = "none";
 }
-charLengthZero();
+passwordPlaceholderStyle();
+
+const generateBtn = document.querySelector(".js-generate-password");
+
+function preventBtnFromSubmitting() {
+  generateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+}
+preventBtnFromSubmitting();
+
+function generatePassword() {
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "~`!@#$%^&*()_-+={[}]|:;/\"'<,>.?/";
+
+  generateBtn.addEventListener("click", () => {
+    const charLength = Number(charLengthSlider.value);
+    const passCheckboxes = document.querySelectorAll(".js-pass-checkbox");
+    let checked = false;
+    const passwordSpecs = [];
+    let charSet = "";
+
+    passCheckboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        checked = true;
+
+        const { passSpecs } = checkbox.dataset;
+        passwordSpecs.push(passSpecs);
+      }
+    });
+
+    if (charLength === 0 || !checked) {
+      return;
+    } else {
+      passwordSpecs.forEach((specs) => {
+        if (specs === "uppercase") {
+          charSet += upperCaseLetters;
+        } else if (specs === "lowercase") {
+          charSet += lowerCaseLetters;
+        } else if (specs === "numbers") {
+          charSet += numbers;
+        } else if (specs === "symbols") {
+          charSet += symbols;
+        }
+      });
+    }
+  });
+}
+generatePassword();
