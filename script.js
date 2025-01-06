a2a9da2b59fae161c8d4ac5947a6f10539e35abf
ashashlin <104874547+ashashlin@users.noreply.github.com>
2025-01-06 23:55:10 +0800

    replace char if password does not include checked criteria

--- script.js
+++ script.js
@@ -24,9 +24,79 @@ function preventBtnFromSubmitting() {
 }
 preventBtnFromSubmitting();
 
+function replaceRandomChar(
+  passwordSpecs,
+  specs,
+  specsArray,
+  passwordString,
+  password,
+  previousRandomIndices,
+  uppercaseLettersArray,
+  lowercaseLettersArray,
+  numbersArray,
+  symbolsArray
+) {
+  if (passwordSpecs.includes(specs)) {
+    const containsSpecs = specsArray.some((letter) =>
+      passwordString.includes(letter)
+    );
+
+    if (!containsSpecs) {
+      const categories = {
+        uppercase: [],
+        lowercase: [],
+        numbers: [],
+        symbols: [],
+      };
+
+      password.forEach((char, index) => {
+        if (uppercaseLettersArray.includes(char)) {
+          categories.uppercase.push(index);
+        } else if (lowercaseLettersArray.includes(char)) {
+          categories.lowercase.push(index);
+        } else if (numbersArray.includes(char)) {
+          categories.numbers.push(index);
+        } else if (symbolsArray.includes(char)) {
+          categories.symbols.push(index);
+        }
+      });
+
+      const uniqueIndices = [];
+
+      if (categories.uppercase.length === 1) {
+        uniqueIndices.push(categories.uppercase[0]);
+      }
+      if (categories.lowercase.length === 1) {
+        uniqueIndices.push(categories.lowercase[0]);
+      }
+      if (categories.numbers.length === 1) {
+        uniqueIndices.push(categories.numbers[0]);
+      }
+      if (categories.symbols.length === 1) {
+        uniqueIndices.push(categories.symbols[0]);
+      }
+
+      let randomPassIndex = Math.floor(Math.random() * password.length);
+
+      while (
+        previousRandomIndices.includes(randomPassIndex) ||
+        uniqueIndices.includes(randomPassIndex)
+      ) {
+        randomPassIndex = Math.floor(Math.random() * password.length);
+      }
+
+      previousRandomIndices.push(randomPassIndex);
+
+      const randomCharIndex = Math.floor(Math.random() * specsArray.length);
+
+      password[randomPassIndex] = specsArray[randomCharIndex];
+    }
+  }
+}
+
 function generatePassword() {
-  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   const numbers = "0123456789";
   const symbols = "~`!@#$%^&*()_-+={[}]|:;/\"'<,>.?/";
 
@@ -51,9 +121,9 @@ function generatePassword() {
     } else {
       passwordSpecs.forEach((specs) => {
         if (specs === "uppercase") {
-          charSet += upperCaseLetters;
+          charSet += uppercaseLetters;
         } else if (specs === "lowercase") {
-          charSet += lowerCaseLetters;
+          charSet += lowercaseLetters;
         } else if (specs === "numbers") {
           charSet += numbers;
         } else if (specs === "symbols") {
@@ -68,6 +138,71 @@ function generatePassword() {
 
         password.push(charSet[randomIndex]);
       }
+
+      let passwordString = password.join("");
+      console.log(passwordString);
+
+      const uppercaseLettersArray = uppercaseLetters.split("");
+      const lowercaseLettersArray = lowercaseLetters.split("");
+      const numbersArray = numbers.split("");
+      const symbolsArray = symbols.split("");
+
+      let previousRandomIndices = [];
+
+      replaceRandomChar(
+        passwordSpecs,
+        "uppercase",
+        uppercaseLettersArray,
+        passwordString,
+        password,
+        previousRandomIndices,
+        uppercaseLettersArray,
+        lowercaseLettersArray,
+        numbersArray,
+        symbolsArray
+      );
+
+      replaceRandomChar(
+        passwordSpecs,
+        "lowercase",
+        lowercaseLettersArray,
+        passwordString,
+        password,
+        previousRandomIndices,
+        uppercaseLettersArray,
+        lowercaseLettersArray,
+        numbersArray,
+        symbolsArray
+      );
+
+      replaceRandomChar(
+        passwordSpecs,
+        "numbers",
+        numbersArray,
+        passwordString,
+        password,
+        previousRandomIndices,
+        uppercaseLettersArray,
+        lowercaseLettersArray,
+        numbersArray,
+        symbolsArray
+      );
+
+      replaceRandomChar(
+        passwordSpecs,
+        "symbols",
+        symbolsArray,
+        passwordString,
+        password,
+        previousRandomIndices,
+        uppercaseLettersArray,
+        lowercaseLettersArray,
+        numbersArray,
+        symbolsArray
+      );
+
+      passwordString = password.join("");
+      console.log(passwordString);
     }
   });
 }
